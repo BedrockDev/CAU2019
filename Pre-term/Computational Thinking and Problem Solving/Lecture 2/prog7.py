dbfe18592d2cf4abe32665bab06ce77668876feb
CAU B210 <dev.bedrock@gmail.com>
2019-01-29 16:50:54 +0900

    Added Python file from Lecture 2-3

--- Pre-term/Computational Thinking and Problem Solving/Lecture 2/prog7.py
+++ Pre-term/Computational Thinking and Problem Solving/Lecture 2/prog7.py
@@ -1,10 +1,10 @@
 # 2019-01-29
 
 # money rate calculator
-money = 1000
-rate = 0.05
-year = 3
+money = int(input("Account balance: "))
+rate = int(input("Rate (%): "))
+year = int(input("Simulation year: "))
 
 for i in range(year):
-    money += money*rate
+    money += money*(rate/100)
     print("Year " + str(i + 1) + ", balance is " + str(int(money)))
