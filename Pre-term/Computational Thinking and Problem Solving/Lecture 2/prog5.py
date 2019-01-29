d1e702e2e180f8e1302c535754eb793a0adc7444
CAU B210 <dev.bedrock@gmail.com>
2019-01-29 16:12:32 +0900

    Added Python files from Lecture 2-2

---
+++ Pre-term/Computational Thinking and Problem Solving/Lecture 2/prog5.py
@@ -0,0 +1,8 @@
+# 2019-01-29
+
+from math import pi
+
+# Today is the first day of the week. What is the day 137 days after today?
+print("Day " + str(137%7))
+print("pi " + str(int(pi * 1000)/1000))
+print("pi (rounded to 3rd) " + str(round(pi * 1000)/1000))
---
+++ Pre-term/Computational Thinking and Problem Solving/Lecture 2/prog6.py
@@ -0,0 +1,9 @@
+# 2019-01-29
+
+from math import pi, sin
+
+# examples of using radian values on calculating PI values
+print(sin(pi/6))
+print(sin(pi/4))
+print(sin(pi/3))
+print(sin(pi/2))
