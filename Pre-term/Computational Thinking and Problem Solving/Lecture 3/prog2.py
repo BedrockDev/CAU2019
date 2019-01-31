a8169c57beca670948a082035d03f9f4c4e11be9
= <=>
2019-01-31 16:01:18 +0900

    Added Python files from Lecture 3-2

---
+++ Pre-term/Computational Thinking and Problem Solving/Lecture 3/prog2.py
@@ -0,0 +1,11 @@
+# 2019-01-31
+# number sorting and reading in list
+
+number = [4, 6, 2, 7, 1, 8, 9, 0, 3, 5]
+
+number.sort()
+
+print(number[0])
+print(number[9])
+print(number[len(number) - 1])
+print(number[-1])
