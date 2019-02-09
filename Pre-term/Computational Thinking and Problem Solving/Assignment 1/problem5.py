5eebb48b85765be76bfa8fca929cb7a66121ac49
BERICT-PC <dev.bedrock@gmail.com>
2019-02-09 22:58:45 +0900

    Updated CTPS Assignment 1 problem 5 to learned materials

--- Pre-term/Computational Thinking and Problem Solving/Assignment 1/problem5.py
+++ Pre-term/Computational Thinking and Problem Solving/Assignment 1/problem5.py
@@ -2,51 +2,32 @@
 
 # corrects the user input value
 while True:
-    try:
-        start = int(input("Please input starting column number: "))
-        end = int(input("Please input ending column number: "))
+    start = int(input("Please input starting column number: "))
+    end = int(input("Please input ending column number: "))
 
-        # check for invalid input
-        if start > 9 or start < 1 or end > 9 or end < 1:
-            print("Invalid input - Both column numbers should be between 1 and 9")
-            continue
-        
-        if end - start > 4:
-            print("The difference should be same or smaller than 4\n")
-            continue
-        elif end - start <= 0:
-            print("Ending column should be bigger than starting column\n")
-            continue
-        else:
-            break
-    except:
+    # check for invalid input
+    if start > 9 or start < 1 or end > 9 or end < 1:
+        print("Invalid input - Both column numbers should be between 1 and 9")
         continue
-
-def print_blank(repeat):
-    s = ""
-
-    # creates a string that contain repeat amount of spaces
-    for i in range(int(repeat)):
-        s += " "
-
-    print(s, end='')
+    
+    if end - start > 4:
+        print("The difference should be same or smaller than 4\n")
+        continue
+    elif end - start < 0:
+        print("Ending column should be bigger than starting column\n")
+        continue
+    else:
+        break
 
 window_width = 80 # seems it's default for IDLE on startup
 column_width = 10
 column_count = end - start + 1
-column_margin = 4
-# use below to use all same margin
-# column_margin = (window_width - column_width*column_count) / (column_count + 1)
+column_margin = int((window_width - column_width*column_count) / (column_count + 1))
 
-print()
-print_blank(window_width/2 - 4)
-print("From", start, "to", end)
-print()
+print('\n', " "*int(window_width/2 - 3), start, " to ", end, '\n', sep='')
 
 for i in range(1, 10):
     # print the blanks to start the first column
-    # remove below to use all same margin
-    print_blank((window_width - column_width*column_count - column_margin*column_count)/2)
     
     for j in range(start, end + 1):
         expression = str(j) + " X " + str(i) + " = "
@@ -57,8 +38,7 @@ for i in range(1, 10):
             expression += " "
 
         # print the expression and answer
-        print_blank(column_margin)
-        print(expression + str(answer), end='')
+        print(" "*column_margin, expression, answer, end='', sep='')
 
     # line break
     print()
