5c072619ffa8e11bccd09673dac1beb9f1aa8827
CAU B311 <dev.bedrock@gmail.com>
2019-03-19 15:49:42 +0900

    Added BCP Lecture 2 assignment C files

---
+++ 1st-term/Basic Computer Programming/Lecture 2/Assignment/assignment2-1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n1 = +2147483647; // Max value
+	int n2 = -2147483648; // Min value
+	printf("before overflow : %d\n", n1);
+
+	n1 = n1 + 100; // Overflow occurs
+	printf("after overflow : %d\n", n1);
+	printf("before underflow : %d\n", n2);
+
+	n2 = n2 – 100; // Underflow occurs
+	printf("after underflow : %d\n", n2);
+
+	return 0;
+}
---
+++ 1st-term/Basic Computer Programming/Lecture 2/Assignment/assignment2-2.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main() {
+	char code1 = 'B';
+	char code2 = 66;
+	char code3 = 'C';
+	char code4 = 67;
+	char code5 = 'D';
+	char code6 = 68;
+
+	printf("code1 = %c\n", code1);
+	printf("code2 = %c\n", code2);
+	printf("code3 = %c\n", code3);
+	printf("code4 = %c\n", code4);
+	printf("code5 = %c\n", code5);
+	printf("code6 = %c\n", code6);
+
+	return 0;
+}
