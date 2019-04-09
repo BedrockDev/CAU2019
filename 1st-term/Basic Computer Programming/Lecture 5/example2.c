33369a1db3fc87e3adbac0b3631abdfae5cbb8b2
CAU 310 B311 <dev.bedrock@gmail.com>
2019-04-09 15:17:32 +0900

    Added BCP Lecture 5 c files

---
+++ 1st-term/Basic Computer Programming/Lecture 5/example2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int i = 0; // initialization
+
+	do {
+		printf("Hello, world! %d\n", i); // repeat
+		i++; // repeat
+	} while (i < 100); // condition
+
+	return 0;
+}
